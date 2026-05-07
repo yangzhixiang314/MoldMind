13358e60696e5f0fbdbc1b7b90e4cd5dc0896b66
jason <13266039884@163.com>
2026-05-07 16:44:37 +0800

    third commit

--- test/test_colpali.py
+++ test/test_colpali.py
@@ -24,5 +24,6 @@ processor = ColQwen2_5_Processor.from_pretrained(local_model_path, local_files_o
 
 print("✅ 模型加载成功！")
 print("✅ 模型加载成功！")
+print("✅ 模型加载成功3！")
 print(f"使用设备: {device}")
 print(f"模型路径: {local_model_path}")
